1a2c8c47a494d8c929011332ff4499019e611d57
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-03-01 22:33:29 +0300

    Create hi and bye

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 import cfg
 
+vk_session = vk_api.VkApi(token=cfg.TOKEN)
+long_poll = VkLongPoll(vk_session)
 
-vk_session = vk_api.VkApi(cfg.TOKEN, cfg.PASSWORD)
-vk_session.auth()
 
-vk = vk_session.get_api()
+def write_msg(user_id, message):
+    try:
+        vk_session.method('messages.send', {'user_id': user_id, 'message': message})
+    except:
+        print('Trouble')
 
-print(vk.wall.post(message='Hello world!'))
+
+for event in long_poll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            request = event.text
+            if request == "Hi":
+                write_msg(event.user_id, "Hi")
+            elif request == "bye":
+                write_msg(event.user_id, "bye((")
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...")
