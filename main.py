2ca6ad9b37726ba0087f4c93289837d591767d5c
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-03-02 11:29:24 +0300

    Add new par random_id

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import cfg
+import random as rand
 
 vk_session = vk_api.VkApi(token=cfg.TOKEN)
 long_poll = VkLongPoll(vk_session)
 
 
 def write_msg(user_id, message):
+    random_id = vk_api.utils.get_random_id()
     try:
-        vk_session.method('messages.send', {'user_id': user_id, 'message': message})
+        vk_session.method('messages.send', {'user_id': user_id, 'message': message, "random_id": random_id})
     except Exception as exc:
         print(exc)
 
