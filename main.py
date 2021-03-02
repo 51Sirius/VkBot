507a2c70b69948a27f27d62903cafa6e48fc5bfc
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-03-02 12:32:24 +0300

    add vk bot class to new file

--- main.py
+++ main.py
@@ -1,40 +1,11 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import cfg
-import bs4
-import requests
+from vk_bot import VkBot
+
 
 vk_session = vk_api.VkApi(token=cfg.TOKEN)
 long_poll = VkLongPoll(vk_session)
-
-
-class VkBot:
-    def __init__(self, user_id):
-        self._USER_ID = user_id
-        self._USERNAME = self._get_user_name_from_vk_id()
-        self._COMMANDS = ["", "", ""]
-        print(f'Create bot for {self._USERNAME}')
-
-    def _get_user_name_from_vk_id(self):
-        req = requests.get("https://vk.com/id" + str(self._USER_ID))
-        bs = bs4.BeautifulSoup(req.text, "html.parser")
-        user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
-        return user_name.split()[0]
-
-    @staticmethod
-    def _clean_all_tag_from_str(string_line):
-        result = ""
-        not_skip = True
-        for i in list(string_line):
-            if not_skip:
-                if i == "<":
-                    not_skip = False
-                else:
-                    result += i
-            else:
-                if i == ">":
-                    not_skip = True
-        return result
 
 
 def write_msg(user_id, message):
@@ -48,10 +19,5 @@ def write_msg(user_id, message):
 for event in long_poll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
-            request = event.text
-            if request == "Hi":
-                write_msg(event.user_id, "Hi")
-            elif request == "bye":
-                write_msg(event.user_id, "bye((")
-            else:
-                write_msg(event.user_id, "Не понял вашего ответа...")
+            message = event.text
+            bot = VkBot(event.user_id, message)
---
+++ vk_bot.py
@@ -0,0 +1,37 @@
+import bs4
+import requests
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+class VkBot:
+    def __init__(self, user_id, message):
+        self.message = message
+        self._USER_ID = user_id
+        self._USERNAME = self._get_user_name_from_vk_id()
+        self._COMMANDS = ["", "", ""]
+        print(f'Create bot for {self._USERNAME}')
+
+    def _get_user_name_from_vk_id(self):
+        req = requests.get("https://vk.com/id" + str(self._USER_ID))
+        bs = bs4.BeautifulSoup(req.text, "html.parser")
+        user_name = self._clean_all_tag_from_str(bs.findAll("title")[0])
+        return user_name.split()[0]
+
+    @staticmethod
+    def _clean_all_tag_from_str(string_line):
+        result = ""
+        not_skip = True
+        for i in list(string_line):
+            if not_skip:
+                if i == "<":
+                    not_skip = False
+                else:
+                    result += i
+            else:
+                if i == ">":
+                    not_skip = True
+        return result
+
+    def command(self):
+        pass
