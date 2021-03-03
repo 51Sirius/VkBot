edd50da8469eedfd7e013e52a07e6b22d471b144
Belaz <belaz178@mail.ru>
2021-03-03 23:34:46 +0300

    add parsing lines

--- parser_space.py
+++ parser_space.py
@@ -1,10 +1,12 @@
 import bs4
 import requests
+import random as rand
 
 
 def parsing_facts():
     req = requests.get('http://interesnyjfakt.ru/top-100-interesnyx-faktov-o-kosmose/')
-    bs: str = bs4.BeautifulSoup(req.text, "html.parser")
-    result = ""
+    bs: bs4.BeautifulSoup = bs4.BeautifulSoup(req.text, "html.parser")
+    bs = bs.findAll('ul')[1:18]
+    result = []
     for i in list(bs):
         pass
