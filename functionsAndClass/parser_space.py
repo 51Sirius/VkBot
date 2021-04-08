ccce73682384f06281e5a5891f95ad508d9cf2b0
51Sirius <71227522+51sirius@users.noreply.github.com>
2021-04-08 13:35:09 +0300

    Error photo

--- functionsAndClass/parser_space.py
+++ functionsAndClass/parser_space.py
@@ -68,8 +68,11 @@ def give_space():
 
 
 def parsing_avatar(id_vk, name):
-    req = requests.get(f'https://vk.com/id{id_vk}')
-    bs: bs4.BeautifulSoup = bs4.BeautifulSoup(req.text, "html.parser")
-    link: bs4.element.Tag = bs.findAll('img', attrs={'class': 'pp_img'})[0].get('src', '-')  # url -> img 100x100
-    print(link, name, sep='\n')
+    try:
+        req = requests.get(f'https://vk.com/id{id_vk}')
+        bs: bs4.BeautifulSoup = bs4.BeautifulSoup(req.text, "html.parser")
+        link: bs4.element.Tag = bs.findAll('img', attrs={'class': 'pp_img'})[0].get('src', '-')  # url -> img 100x100
+        print(link, name, sep='\n')
+    except Exception as e:
+        link = 'https://mobile-zip.ru/wp-content/uploads/f/d/9/fd92001ea87bcdbf9c92be70a1000043.jpeg'
     return link
